793524e69c71d03f83596412ee9efb9738584d89
jwfwessels <jwfwessels@gmail.com>
2019-12-08 19:15:18 +0100

    add more conditional logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,21 @@ echo "Setting up your Mac 🛠 🏗"
 # sudo softwareupdate -i -a
 
 echo "Install Apple command line tools..."
-xcode-select --install
+# untested
+check="$(xcode-\select --install)"
+echo "$check"
+str="xcode-select: note: install requested for command line developer tools\n"
+while [[ "$check" == "$str" ]]; do
+    check="$(xcode-\select --install)"
+    sleep 1
+done
 
-echo "clone repo"
-git clone --recursive https://github.com/jwfwessels/dotfiles.git $HOME/.dotfiles
+if [[ ! -d "$HOME/.dotfiles" ]]; then
+    echo "clone repo"
+    git clone --recursive https://github.com/jwfwessels/dotfiles.git $HOME/.dotfiles
+else
+    echo "~/.dotfiles already exists..."
+fi
 
 echo "move to repo"
 cd ~/.dotfiles
@@ -35,7 +46,7 @@ ln -s $HOME/.dotfiles/settings/Spectacle/Shortcuts.json $HOME/Library/Applicatio
 code --install-extension Shan.code-settings-sync
 
 # install ohmyzsh
-if test !$(which ZSH); then
+if [[ $(command -v ZSH) == "" ]]; then
     echo "Seems we dont have OH MY ZSH 😱"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     zsh
@@ -43,22 +54,6 @@ if test !$(which ZSH); then
     ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 fi
-
-do_install_font_assets() {
-  print_subsection "Installing fonts ✏️"
-  if $IS_MAC; then
-    _font_dir="${HOME}/Library/Fonts"
-    [[ -d "${_font_dir}" ]] || mkdir -p "${_font_dir}"
-    cp -f -v -R "${PROJECT_REPOSITORY_DIR}"/assets/fonts/* "${_font_dir}/"
-    print_last_command_status
-#   elif $IS_BASED_ON_UBUNTU_1604 || $IS_ARCH; then
-#     _font_dir="/usr/local/share/fonts"
-#     cp -f -v -r "${PROJECT_REPOSITORY_DIR}"/assets/fonts/* "${_font_dir}/" && fc-cache -f -v
-#     fc-list | grep "$(ls "${_font_dir}" | sort -n | head -1)" # confirm if fonts have been installed
-#     print_last_command_status
-  fi
-  unset _font_dir
-}
 
 echo "last step switch this repo to using ssh 😬"
 cd $HOME/.dotfiles && git remote set-url origin git@github.com:jwfwessels/dotfiles.git
